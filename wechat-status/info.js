f52db09d48b9217e8af3813545e398a617bbaabd
Yang Wen <kevin.wenyang@gmail.com>
2017-05-27 22:47:38 +0800

    update info

--- wechat-status/info.js
+++ wechat-status/info.js
@@ -1,23 +1,29 @@
-/*用户加机器人好友状态：数据库创建用户新状态，并给用户返回文本信息*/
+/*用户已加机器人为好友状态：数据库创建用户新状态，并给用户返回文本信息*/
 const UserStatus = require('../model/userStatus');
+const Constant = require('../config/constant');
 
 class Info {
   showText() {
     return {
       type: 'Text', info: `您好，请选择您的角色[请回复数字]:
     1.我想当学员
-    2.我想当助教
+    2.我想当助教或者志愿者
     3.我想合作`
     };
   }
 
   handler(userId, message, callback) {
-    UserStatus.create({userId: userId, status: 'choice'}, (err) => {
-      if (err) {
-        return callback(err, null);
-      }
-      return callback(null, this.showText());
-    });
+    if (message.type === 'Note' && message.text.includes('现在可以开始聊天了')){
+      UserStatus.create({userId: userId, status: 'choice'}, (err) => {
+        if (err) {
+          return callback(err, null);
+        }
+        return callback(null, this.showText());
+      });
+    } else {
+      return callback(null, Constant.validate.no);
+    }
+
   }
 }
 
