556b65af0a4c8d76a7d88a822e03e0efc2e3e621
Yang Wen <kevin.wenyang@gmail.com>
2017-10-17 16:50:11 +0800

    update questions

--- config/constant.js
+++ config/constant.js
@@ -1,22 +1,23 @@
 const constant = {
-  httpCode: {
-    OK: 200,
-    CREATED: 201,
-    NO_CONTENT: 204,
-    NOT_FOUND: 404,
-    BAD_REQUEST: 400
-  },
-  validate: {
-    info: {
-      type: 'Text', info: `请选择您的角色[请输入序号]:
+    httpCode: {
+        OK: 200,
+        CREATED: 201,
+        NO_CONTENT: 204,
+        NOT_FOUND: 404,
+        BAD_REQUEST: 400
+    },
+    validate: {
+        info: {
+            type: 'Text',
+            info: `回答完问题后会拉你入群，请选择您的角色[请输入序号]:
     1.我要当学员
     2.我要当助教
     3.我想合作`
-    },
-    err: {type: 'Text', info: '输入格式有误,请按要求重新输入；或输入q重新回答'},
-    incorrect: {type: 'Text', info: '输入格式有误'},
-    no: {type: 'Null', info: ''},
-    end: {type: 'Text', info: '主题讨论终止'}
-  }
+        },
+        err: { type: 'Text', info: '输入格式有误,请按要求重新输入；或输入q重新回答' },
+        incorrect: { type: 'Text', info: '输入格式有误' },
+        no: { type: 'Null', info: '' },
+        end: { type: 'Text', info: '主题讨论终止' }
+    }
 };
 module.exports = constant;
